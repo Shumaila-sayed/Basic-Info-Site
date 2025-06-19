042d7aa2e819a690037222fae4a027b0e35e5f2f
Shumaila <sayedshumaila27@gmail.com>
2025-06-20 01:07:10 +0530

    rewrite through express

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
-import http from 'http';
+import express from 'express';
+// import http from 'http';
 import url from 'url';
 import path from 'path';
 import fs from 'fs/promises';
-const PORT = 8080;
+const app = express();
+const PORT = 3000;
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const server = http.createServer(async(req, res) => {
-	let filePath;
-	if (req.url === '/') {
-		filePath = path.join(__dirname, 'index.html');
-	} else if (req.url === '/about') {
-		filePath = path.join(__dirname, 'about.html');
-	} else if (req.url === '/contact-me') {
-		filePath = path.join(__dirname, 'contact-me.html');
-	} else {
-		filePath = path.join(__dirname, '404.html');
-    }
-    
-    const data = await fs.readFile(filePath);
-    res.setHeader('Content-type', 'text.html');
-    res.write(data);
-    res.end();
+// const server = http.createServer(async(req, res) => {
+// 	let filePath;
+// 	if (req.url === '/') {
+// 		filePath = path.join(__dirname, 'index.html');
+// 	} else if (req.url === '/about') {
+// 		filePath = path.join(__dirname, 'about.html');
+// 	} else if (req.url === '/contact-me') {
+// 		filePath = path.join(__dirname, 'contact-me.html');
+// 	} else {
+// 		filePath = path.join(__dirname, '404.html');
+//     }
+
+//     const data = await fs.readFile(filePath);
+//     res.setHeader('Content-type', 'text.html');
+//     res.write(data);
+//     res.end();
+// });
+
+const serveHTMl = async (res, fileName) => {
+	try {
+		const filePath = path.join(__dirname, fileName);
+		const data = await fs.readFile(filePath);
+		res.setHeader('Content-type', 'text/html');
+		res.send(data);
+	} catch (error) {
+		res.status(500).send('Server Error');
+	}
+};
+
+app.get('/', async (req, res) => {
+	await serveHTMl(res, 'index.html');
 });
 
-server.listen(PORT, () => {
-    console.log(`Listening to the port ${PORT}`);
-    
-})
+app.get('/about', async (req, res) => {
+	await serveHTMl(res, 'about.html');
+});
+
+app.get('/contact-me', async (req, res) => {
+	await serveHTMl(res, 'contact-me.html');
+});
+
+app.use(async (req, res) => {
+	await serveHTMl(res, '404.html');
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening to the port ${PORT}`);
+});
